3deaf6180a3cf2331ac2e61498632ec822431f01
johan-franklin <87813497+johan-franklin@users.noreply.github.com>
2022-04-26 18:00:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+mustache_x=90;
+mustache_y=90;
+
+function preload() {
+  
+       mustache= loadImage('https://i.postimg.cc/wMcQVGHY/R-1.png');
+        }
+        
+
+
+
+function setup() {
+    canvas = createCanvas(300,300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function draw(){
+    image(video,0,0,300,300);
+    image(mustache,mustache_x,mustache_y,60,45);
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
+function modelLoaded(){
+    console.log("poseNet Is Initialized"); 
+}
+
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+       
+        console.log("nose x = " +results[0].pose.nose.x);
+     mustache_x = results[0].pose.nose.x-30;
+        console.log("nose y = " +results[0].pose.nose.y);
+    }
+
+
+}
